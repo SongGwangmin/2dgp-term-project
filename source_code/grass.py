366264e2e452e98679227f64394a033d26a7ce07
Gwangmin Song <thdalsrhkd@naver.com>
2025-11-10 14:16:52 +0900

    맵 구분 위해 grass y좌표 구분

--- source_code/grass.py
+++ source_code/grass.py
@@ -2,12 +2,12 @@ from pico2d import load_image
 
 
 class Grass:
-    def __init__(self, y=30):
+    def __init__(self, y=0):
         self.image = load_image('grass.png')
         self.y = y
 
     def draw(self):
-        self.image.draw(400, 30)
+        self.image.draw(400, 30 + self.y)
 
     def update(self):
         pass
