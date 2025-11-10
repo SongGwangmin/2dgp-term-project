889f497a6ace3fdc096b4c8d3c4027dc5f005612
Gwangmin Song <thdalsrhkd@naver.com>
2025-11-10 14:30:11 +0900

    모드를 2개 만든다. grass의 좌표로 임시로 구분한다

---
+++ source_code/worldmap_mode.py
@@ -0,0 +1,54 @@
+from pico2d import *
+from boy import Boy
+from grass import Grass
+import game_world
+import game_framework
+from game_world import world
+# Game object class here
+
+
+def handle_events():
+    global running
+
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+            #game_framework.change_mode(title_mode)
+        #elif event.type == SDL_KEYDOWN and event.key == SDLK_i:
+            #game_framework.push_mode(item_mode)
+            # push mode: 플레이 모드를 유지해야하므로
+        else:
+            boy.handle_event(event)
+
+
+def init():
+    global boy
+    global running
+    running = True
+    grass = Grass(30)
+    game_world.add_object(grass, 0)
+    boy = Boy()
+    game_world.add_object(boy, 1)
+
+
+def finish():
+    game_world.clear()
+
+def update():
+    game_world.update()
+
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+
+def pause():
+    pass
+
+def resume():
+    pass
