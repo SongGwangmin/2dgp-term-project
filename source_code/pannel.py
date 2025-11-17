171231bb231948b72085308e95b63124a7e4a487
Gwangmin Song <thdalsrhkd@naver.com>
2025-11-18 00:20:59 +0900

    mousemove_colision으로 어느 부분 활성화할지 결정

--- source_code/pannel.py
+++ source_code/pannel.py
@@ -8,31 +8,42 @@ class Pannel:
             Pannel.image = load_image('item_select.png')
         self.Sold_out = load_image('sold_out.png')
         self.divider = divider
-        self.slashleft = 270
-        self.slashright = 530
-        self.slashbottom = 200
-        self.slashtop = 260
+        # border[0] = (left, bottom, right, top)
+        # border[1] = border[0]의 각 요소에 100을 더한 값
+        self.border = (
+            (270, 200, 530, 260),
+            (270, 260, 530, 320)
+        )
+        self.cursorin = 0
 
     def draw(self):
         self.image.draw(400, 300)
         if self.divider == 0:
+            if self.cursorin == 1:
+                draw_rectangle(*self.border[0])
+            elif self.cursorin == 2:
+                draw_rectangle(*self.border[1])
+                
             if boy.Boy.set_slash == 1:
                 self.Sold_out.draw(700, 150)
-            else:
-                draw_rectangle(self.slashleft, self.slashbottom, self.slashright, self.slashtop)
+
+
 
 
     def update(self):
         pass
 
     def click_colision(self, x, y):
-        if self.divider == 0:
-            if self.slashleft <= x <= self.slashright and self.slashbottom <= y <= self.slashtop:
-                return True
-        return False
+        if self.border[0][0] <= x <= self.border[0][2] and self.border[0][1] <= y <= [3]:
+            return 1
+        elif self.border[1][0] <= x <= self.border[1][2] and self.border[1][1] <= y <= self.border[1][3]:
+            return 2
+        return 0
 
     def mousemove_colision(self, x, y):
-        if self.divider == 0:
-            if self.slashleft <= x <= self.slashright and self.slashbottom <= y <= self.slashtop:
-                return True
-        return False
+        if self.border[0][0] <= x <= self.border[0][2] and self.border[0][1] <= y <= self.border[0][3]:
+            self.cursorin = 1
+        elif self.border[1][0] <= x <= self.border[1][2] and self.border[1][1] <= y <= self.border[1][3]:
+            self.cursorin = 2
+        else:
+            self.cursorin = 0
