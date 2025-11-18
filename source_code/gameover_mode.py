09e8453dc24ba0e3fc9fc0ab789af280106cbe86
Gwangmin Song <thdalsrhkd@naver.com>
2025-11-18 23:59:28 +0900

    death일 때 boy에 핸들이벤트를 넘겨서 오류 해결

--- source_code/gameover_mode.py
+++ source_code/gameover_mode.py
@@ -4,9 +4,24 @@ from plate import Plate
 import game_world
 import home_mode
 
+def find_boy():
+    """game_world.world를 순회하여 클래스 이름이 'Boy'인 객체를 찾아 반환합니다.
+    다른 모듈과의 순환 import 문제를 피하기 위해 __class__.__name__으로 비교합니다.
+    이 함수는 호출만 할 뿐, 자동으로 다른 곳에서 호출되지는 않습니다.
+    """
+    for layer in game_world.world:
+        for o in layer:
+            if o.__class__.__name__ == 'Boy':
+                return o
+    return None
+
+boy = None
+
 def init():
     plate = Plate()
     game_world.add_object(plate, 3)
+    global boy
+    boy = find_boy()
 
 def finish():
     game_world.clear()
@@ -24,6 +39,7 @@ def draw():
     pass
 
 def handle_events():
+    global boy
     event_list = get_events()
 
     for event in event_list:
@@ -32,6 +48,8 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_r:
             game_framework.pop_mode()
             game_framework.change_mode(home_mode)
+        else:
+            boy.handle_event(event)
 
 
 
