02377f13e1d112303253c6e9cd191bd82cc42dbf
Gwangmin Song <thdalsrhkd@naver.com>
2025-11-20 01:42:45 +0900

    훈련장 생성

---
+++ source_code/trainig_mode.py
@@ -0,0 +1,71 @@
+from pico2d import *
+import game_framework
+from pannel import Pannel
+from sold_out import Sold_out
+import game_world
+import play_mode
+import home_mode
+import worldmap_mode
+
+def init():
+    global pannel
+    pannel = Pannel(0)
+    Pannel.image = load_image('training_select.png')
+    game_world.add_object(pannel, 2)
+
+def finish():
+
+    global pannel
+    game_world.remove_object(pannel)
+    del pannel
+
+def update():
+    game_world.update()
+    game_world.handle_collision()
+    # 맵 이동 - 오른쪽 끝 == 월드맵
+    if home_mode.boy.x > 790:
+        game_framework.pop_mode()
+        game_framework.change_mode(worldmap_mode)
+
+    # 아이템 모드에서 플레이모드가 유지가 되어야 하므로 게임 월드를 계속 업데이트 해줘야한다
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+    pass
+
+def handle_events():
+    event_list = get_events()
+
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.pop_mode()
+        elif event.type  == SDL_MOUSEBUTTONDOWN:
+            x, y = event.x, 600 - event.y
+            select = pannel.click_colision(x, y)
+            if select == 1:
+                if home_mode.Boy.money >= 3:
+                    home_mode.Boy.money -= 3
+                    home_mode.Boy.max_hp += 10
+            if select == 2:
+                if home_mode.Boy.money >= 3:
+                    home_mode.Boy.money -= 3
+                    home_mode.Boy.strength += 1
+            elif select == 3:
+                game_framework.pop_mode()
+
+        elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, 600 - event.y
+            pannel.mousemove_colision(x, y)
+        else:
+            home_mode.boy.handle_event(event)
+
+
+def pause():
+    pass
+
+def resume():
+    pass
