52a48c259c721d202d89d3c5432c4713ac972287
Gwangmin Song <thdalsrhkd@naver.com>
2025-11-20 14:09:37 +0900

    2스테이지 복제

--- source_code/worldmap_mode.py
+++ source_code/worldmap_mode.py
@@ -5,6 +5,7 @@ import game_world
 import game_framework
 import home_mode
 import play_mode
+import chaser_mode
 from game_world import world
 from keycap import Keycap
 
@@ -65,16 +66,20 @@ def init():
     dest_list.clear()
     key1 = Keycap(30, 200, dest=home_mode)
     key2 = Keycap(250, 200, dest=play_mode)
+    key3 = Keycap(470, 200, dest=chaser_mode)
     game_world.add_object(key1, 3)
     game_world.add_object(key2, 3)
+    game_world.add_object(key3, 3)
     dest_list.append(key1)
     dest_list.append(key2)
+    dest_list.append(key3)
 
     # boy와 Keycap 간 충돌 페어 등록: 'boy:portal'
     # 패턴: 먼저 boy를 왼쪽에 등록하고, 우측 객체들을 별도로 등록
     game_world.add_collision_pair('boy:portal', boy, None)
     game_world.add_collision_pair('boy:portal', None, key1)
     game_world.add_collision_pair('boy:portal', None, key2)
+    game_world.add_collision_pair('boy:portal', None, key3)
 
 
 def finish():
