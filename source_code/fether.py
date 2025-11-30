cad0d8e69e31410f416a0901daf02a4f4b73f997
Gwangmin Song <thdalsrhkd@naver.com>
2025-12-01 00:28:05 +0900

    strength 추가하고 boy:enemy로 통일

--- source_code/fether.py
+++ source_code/fether.py
@@ -16,12 +16,13 @@ RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
 class Fether:
     image = None
-    def __init__(self, x = 400, y=400, boy = None):
+    def __init__(self, x = 400, y=400, boy = None, strength=5):
         print('create fether')
         if Fether.image is None:
             Fether.image = load_image('fether.png')  # 적절한 이미지로 교체 가능
         self.x = x
         self.y = y
+        self.strength = strength
 
         # boy의 현재 좌표를 목표로 설정
         if boy:
@@ -31,7 +32,7 @@ class Fether:
             # boy를 향한 방향 계산 (각도)
             dx = self.target_x - self.x
             dy = self.target_y - self.y
-            self.angle = math.atan2(dy, dx)  # 반대 방향으로 날아가도록 각도 조정
+            self.angle = math.atan2(dy, dx)
             distance = math.sqrt(dx**2 + dy**2)
 
         else:
@@ -39,7 +40,7 @@ class Fether:
             self.dir_y = 0
 
     def draw(self):
-        self.image.clip_composite_draw(0, 0, 64, 64, self.angle + math.pi / 2, 'h', self.x, self.y, METER * PIXEL_PER_METER, METER * PIXEL_PER_METER)
+        self.image.clip_composite_draw(0, 0, 64, 64, self.angle + math.pi / 2, '', self.x, self.y, METER * PIXEL_PER_METER, METER * PIXEL_PER_METER)
         draw_rectangle(*self.get_bb())
 
     def update(self):
@@ -55,7 +56,7 @@ class Fether:
         return self.x - 15, self.y - 15, self.x + 15, self.y + 15
 
     def handle_collision(self, group, other):
-        if group == 'fether:boy':
+        if group == 'boy:enemy':
             game_world.remove_object(self)
             print('fether collide with boy')
 
