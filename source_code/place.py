0f4feddddc6757069a10e30f3334287c3fdb2a61
Gwangmin Song <thdalsrhkd@naver.com>
2025-12-09 18:59:16 +0900

    보스전 전용 바닥

---
+++ source_code/place.py
@@ -0,0 +1,29 @@
+from pico2d import *
+import common
+
+class Place:
+    image = None
+    def __init__(self, y=0, x=800):
+        #self.image = load_image('forest.png')
+        self.y = y
+        if Place.image is None:
+            Place.image = load_image('grass.png')
+        self.cw = get_canvas_width()
+
+        self.w = self.image.w
+        self.left = 0
+
+
+
+    def draw(self):
+        self.image.clip_draw_to_origin( self.left, 0, self.cw, 600, 0, 0 )
+        #draw_rectangle(*self.get_bb())
+
+    def update(self):
+        self.left = clamp(0, int(common.boy.x) - self.cw // 2, self.w - self.cw - 1)
+
+    def get_bb(self):
+        return 0, 0, 8000, 50
+
+    def handle_collision(self, group, other):
+        pass
